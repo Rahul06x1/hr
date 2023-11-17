ba07356fa3f33835132edac10ef5fff13b59d8d2
Rahul Rajeev <rahulrajeev2k1@gmail.com>
2023-11-17 17:45:18 +0530

    Create database and table through python

--- hr.py
+++ hr.py
@@ -2,6 +2,7 @@ import argparse
 import csv
 import logging
 import os
+import psycopg2
 import requests
 
 logger = None
@@ -9,9 +10,11 @@ logger = None
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        prog="hr.py", description="Generates vcard and qrcode for each employee from csv file"
+        prog="hr.py",
+        description="Generates vcard and qrcode for each employee from csv file",
     )
-    parser.add_argument("csv_file", help="name of csv file containing employee details")
+    parser.add_argument('mode', help="action to perform", choices=['generate', 'initdb', 'load'],nargs="?",default="generate")
+    parser.add_argument("-c","--csv_file",action="store",type=str)
     parser.add_argument(
         "-o",
         "--overwrite",
@@ -34,7 +37,7 @@ def parse_args():
         "--qrcodedimension",
         help="set custom qr code dimensions",
         type=int,
-        choices=range(70, 548)
+        choices=range(70, 548),
     )
     parser.add_argument(
         "-r",
@@ -92,22 +95,111 @@ def setup_logging(log_level):
     logger.addHandler(handler)
     logger.addHandler(fhandler)
 
+def check_database_exist():
+    conn = psycopg2.connect("dbname=postgres user=hamon")
+    cur = conn.cursor()
+    conn.autocommit = True
+    cur.execute("""
+        SELECT EXISTS (
+            SELECT datname
+            FROM pg_catalog.pg_database
+            WHERE lower(datname) = lower('hr')
+        );
+    """)
+    exists = cur.fetchone()[0]
+    cur.close()
+    conn.close()
+    return exists
 
-def get_csv_data(file_name, lines, start=0, end=float("inf")):
+def check_table_row_exist():
+    conn = psycopg2.connect("dbname=hr user=hamon")
+    cur = conn.cursor()
+    conn.autocommit = True
+    cur.execute("""
+        SELECT EXISTS (
+            SELECT 1
+            FROM employees
+        );
+    """)
+    exists = cur.fetchone()[0]
+    cur.close()
+    conn.close()
+    return exists
+
+def initialize_table():
+    conn = psycopg2.connect("dbname=hr user=hamon")
+    cur = conn.cursor()
+    cur.execute(
+            "CREATE TABLE employees (id SERIAL PRIMARY KEY, last_name VARCHAR(50) NOT NULL, first_name VARCHAR(50) NOT NULL, designation VARCHAR(50) NOT NULL,email VARCHAR(50) NOT NULL UNIQUE, phone VARCHAR(50) NOT NULL);"
+        )
+    conn.commit()
+    logger.info("Table initialized")
+    cur.close()
+    conn.close()
+
+def initialize_database():
+    conn = psycopg2.connect("dbname=postgres user=hamon")
+    cur = conn.cursor()
+    conn.autocommit = True
+    cur.execute("""
+        SELECT EXISTS (
+            SELECT datname
+            FROM pg_catalog.pg_database
+            WHERE lower(datname) = lower('hr')
+        );
+    """)
+    exists = cur.fetchone()[0]
+    if exists:
+        logger.warning("Database 'hr' already exists")
+    else:
+        cur.execute(
+            "CREATE DATABASE hr;"
+        )
+        logger.info("Database initialized")
+        
+    cur.close()
+    conn.close()
+
+
+def insert_data_to_database_table(lname, fname, designation, email, phone):
+    conn = psycopg2.connect("dbname=hr user=hamon")
+    cur = conn.cursor()
+    cur.execute(
+        "INSERT INTO employees (last_name, first_name, designation, email, phone) VALUES (%s, %s, %s, %s, %s)",
+        (lname, fname, designation, email, phone),
+    )
+    conn.commit()
+    cur.close()
+    conn.close()
+
+def get_table_data(start=1, end=9223372036854775807):
+    offset = start-1
+    limit = end - start + 1
+    conn = psycopg2.connect("dbname=hr user=hamon")
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT last_name, first_name, designation, email, phone FROM employees LIMIT %s OFFSET %s;",(limit, offset)
+    )
+    lines = cur.fetchall()
+    print(len(lines))
+    cur.close()
+    conn.close()
+    return lines
+
+def get_csv_data(file_name, lines):
     line_num = 0
     with open(file_name, mode="r") as file:
         csv_file = csv.reader(file)
         for l in csv_file:
             line_num += 1
-            if start <= line_num <= end:
-                if len(l) == 5:
-                    lines.append(l)
-                else:
-                    logger.warning(
-                        "Insufficient data provided in line %d of file %s",
-                        line_num,
-                        file_name,
-                    )
+            if len(l) == 5:
+                lines.append(l)
+            else:
+                logger.warning(
+                    "Insufficient data provided in line %d of file %s",
+                    line_num,
+                    file_name,
+                )
     return lines
 
 
@@ -143,15 +235,52 @@ END:VCARD
 
 
 def main():
-    args = parse_args()
     row_count = 0
+    args = parse_args()
     if args.verbose:
         setup_logging(logging.DEBUG)
     else:
         setup_logging(logging.INFO)
-    if not os.path.isfile(args.csv_file):
-        logger.error("%s does not exists", args.csv_file)
+        
+    if args.mode == 'initdb':
+        initialize_database()
+        initialize_table()
         exit()
+    elif args.mode == 'load':
+        database_exist = check_database_exist()
+        if not database_exist:
+            logger.error("""
+Database 'hr' does not exist
+Use initdb to initialize database
+""")
+            exit()
+        if not args.csv_file:
+            logger.error("Provide csv file for loading using -c")
+        else:
+            if not os.path.isfile(args.csv_file):
+                logger.error("%s does not exists", args.csv_file)
+                exit()
+            lines = get_csv_data(args.csv_file, [])
+            for lname, fname, designation, email, phone in lines:
+                insert_data_to_database_table(lname, fname, designation, email, phone)
+            logger.info("csv file loaded")
+        exit()
+     
+    database_exist = check_database_exist()
+    if not database_exist:
+        logger.error("""
+Database 'hr' does not exist
+Use initdb to initialize database
+""")
+        exit()
+    table_row_exist = check_table_row_exist()
+    if not table_row_exist:
+        logger.warning("""
+Table 'employees' does not have any data
+Use load to load csv file data to table
+""")
+        exit()
+
     if not os.path.exists(args.directory):
         os.makedirs(args.directory)
     elif not args.overwrite:
@@ -167,13 +296,15 @@ Use -o to overwrite
             logger.error("Use only 2 arguments")
             exit()
         start, end = args.range
-        lines = get_csv_data(args.csv_file, [], start, end)
+        if start > end:
+            logger.error("start should be less than end")
+            exit()
+        lines = get_table_data(start, end)
     else:
-        lines = get_csv_data(args.csv_file, [])
+        lines = get_table_data()
 
-    for line in lines:
+    for lname, fname, designation, email, phone in lines:
         row_count += 1
-        lname, fname, designation, email, phone = line
         data = generate_vcf_data(lname, fname, designation, email, phone, args.address)
         if args.qrcodedimension:
             dimension = args.qrcodedimension
