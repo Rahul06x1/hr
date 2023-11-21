1ccb739c8fdfc891fb7d83578d10e2e8c4cf28b9
Rahul Rajeev <rahulrajeev2k1@gmail.com>
2023-11-21 15:55:05 +0530

    Implement cascade for auto deletion

--- sql/initialize_leave_table.sql
+++ sql/initialize_leave_table.sql
@@ -1,3 +1,4 @@
 CREATE TABLE leaves (id SERIAL PRIMARY KEY, date DATE NOT NULL, 
-        reason VARCHAR(200) NOT NULL, employee_id INTEGER references employees(id),
+        reason VARCHAR(200) NOT NULL,
+        employee_id INTEGER references employees(id) ON DELETE CASCADE,
         UNIQUE(employee_id, date));
